b8edcee265c4ce9d57ab0cd11a2d4e9a342d1559
Garrett Tolbert <garrett1.tolbert@gmail.com>
2022-05-03 19:17:38 -0500

    feat: added tailwind config for ui library

---
+++ packages/ui/components/atoms/Button.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export const Button = () => {
+  return (
+    <button className="rounded-md bg-red-400 p-3 shadow-md transition-all hover:bg-red-700">
+      Boop
+    </button>
+  );
+};
