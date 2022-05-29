ea71b7e70d9a3d52926bdce32857c0ed7d84f315
Darryl Brooks <darrylbrooks13@gmail.com>
2022-05-29 10:46:54 -0500

    fix: fixing prettier config

--- apps/core/src/pages/index.tsx
+++ apps/core/src/pages/index.tsx
@@ -1,20 +1,19 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => (
-	<div className="h-full w-full bg-black">
-		<div className="h-screen w-screen p-6">
-			{/* Stack */}
-			<div className="h-403 flex w-1/2 flex-col justify-evenly">
-				<h1 className="leading-3.25 text-5xl font-bold text-white">
-					A global community for you, built by people like you.
-				</h1>
-				<h2 className="text-xl text-white ">
-					Now that we know who you are, I know who I am. I&apos;m not
-					a mistake! It all makes sense!
-				</h2>
-			</div>
-		</div>
-	</div>
+  <div className="h-full w-full bg-black">
+    <div className="h-screen w-screen p-6">
+      {/* Stack */}
+      <div className="h-403 flex w-1/2 flex-col justify-evenly">
+        <h1 className="leading-3.25 text-5xl font-bold text-white">
+          A global community for you, built by people like you.
+        </h1>
+        <h2 className="text-xl text-white    ">
+          Now that we know who you are, I know who I am. I&apos;m not a mistake! It all makes sense!
+        </h2>
+      </div>
+    </div>
+  </div>
 );
 
 export default Home;
