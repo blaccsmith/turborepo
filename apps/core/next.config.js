a2a57d42d1bd83694935755449c4b0b44007a27b
Darryl Brooks <darrylbrooks13@gmail.com>
2022-05-29 14:58:39 -0500

    feat: init scaffolding

--- apps/core/next.config.js
+++ apps/core/next.config.js
@@ -4,4 +4,9 @@ const withTM = require('next-transpile-modules')(['ui']);
 
 module.exports = withTM({
   reactStrictMode: true,
+  images: {
+		domains: [
+			'images.unsplash.com',
+		],
+	},
 });
--- apps/core/src/pages/index.tsx
+++ apps/core/src/pages/index.tsx
@@ -1,78 +1,39 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
 import Image from 'next/image';
-import { Button } from 'ui';
+import homePageImages from '../constants';
 
 const Home: NextPage = () => (
-  <div className="flex min-h-screen flex-col items-center justify-center py-2">
-    <Head>
-      <title>Create Next App – with Linear</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-
-    <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-      <h1 className="text-6xl font-bold">
-        Welcome to{' '}
-        <a className="text-blue-600" href="https://nextjs.org">
-          Next.js!
-        </a>
-      </h1>
-      <Button />
-
-      <p className="mt-3 text-2xl">
-        Get started by editing{' '}
-        <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">pages/index.tsx</code>
-      </p>
-
-      <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-        <a
-          href="https://nextjs.org/docs"
-          className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-        >
-          <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-          <p className="mt-4 text-xl">
-            Find in-depth information about Next.js features and its API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn"
-          className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-        >
-          <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-          <p className="mt-4 text-xl">Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
-
-        <a
-          href="https://github.com/vercel/next.js/tree/canary/examples"
-          className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-        >
-          <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-          <p className="mt-4 text-xl">Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-        >
-          <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-          <p className="mt-4 text-xl">
-            Instantly deploy your Next.js site to a public URL with Vercel.
-          </p>
-        </a>
+  <div className="h-full w-full bg-black">
+    <div className="flex h-screen w-screen p-6">
+      {/* Stack */}
+      <div className="flex h-1/2 w-1/2 flex-col justify-center">
+        <h1 className="leading-3.25 mb-10 text-5xl font-bold text-white">
+          A global community for you, built by people like you.
+        </h1>
+        <h2 className="mb-5 text-xl text-white">
+          Now that we know who you are, I know who I am. I&apos;m not a mistake! It all makes sense!
+        </h2>
       </div>
-    </main>
-
-    <footer className="flex h-24 w-full items-center justify-center border-t">
-      <a
-        className="flex items-center justify-center gap-2"
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-      </a>
-    </footer>
+      {/* Grid */}
+      <div className="grid h-1/2 grid-cols-3 grid-rows-3 gap-5">
+        {homePageImages.map((src: string, idx: number) => idx === 4 || idx === 5 ? (
+            <div key={src} className="h-auto w-auto" />
+          ) : (
+            <div className="relative w-44" key={src}>
+              <Image
+                src={src}
+                alt={`black people in tech #${idx}`}
+                width="172px"
+                height="172px"
+                objectFit="cover"
+                style={{
+                  filter: 'grayscale(100%)',
+                }}
+              />
+            </div>
+          ))}
+      </div>
+    </div>
   </div>
 );
 
