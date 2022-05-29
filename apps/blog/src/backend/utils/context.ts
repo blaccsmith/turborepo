4f6d1a1c7383bc8cf999651f8a5c9cc6d2b5e135
Garrett Tolbert <garrett1.tolbert@gmail.com>
2022-05-29 16:18:33 -0500

    cloning trpc template (#3)
    * cloned trpc template
    
    * fix: fixed blog build issue

---
+++ apps/blog/src/backend/utils/context.ts
@@ -0,0 +1,11 @@
+import { getSession } from 'next-auth/react';
+import * as trpc from '@trpc/server';
+import * as trpcNext from '@trpc/server/adapters/next';
+import { prisma } from '@/lib/prisma';
+
+export const createContext = async ({ req, res }: trpcNext.CreateNextContextOptions) => {
+	const session = await getSession({ req });
+	return { req, res, prisma, session };
+};
+
+export type Context = trpc.inferAsyncReturnType<typeof createContext>;
