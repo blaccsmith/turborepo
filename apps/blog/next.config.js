c95dfe33b945d7701d771d85d57bdb2ca32f5839
Garrett Tolbert <garrett1.tolbert@gmail.com>
2022-05-30 16:23:24 -0500

    fix: updated configs with global packages

--- apps/blog/next.config.js
+++ apps/blog/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const withTM = require('next-transpile-modules')(['ui']);
+const withTM = require('next-transpile-modules')(['ui', 'utils', 'types']);
 
 module.exports = withTM({
-	reactStrictMode: true,
+  reactStrictMode: true,
 });
--- apps/core/next.config.js
+++ apps/core/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-const withTM = require('next-transpile-modules')(['ui']);
+const withTM = require('next-transpile-modules')(['ui', 'utils', 'types']);
 
 module.exports = withTM({
   reactStrictMode: true,
