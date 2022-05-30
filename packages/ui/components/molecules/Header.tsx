d70df80328a73de292febe02030c3c4d4de814bb
Garrett Tolbert <garrett1.tolbert@gmail.com>
2022-05-30 16:24:42 -0500

    refactor: working on header

--- packages/ui/components/molecules/Header.tsx
+++ packages/ui/components/molecules/Header.tsx
@@ -4,10 +4,9 @@ const Header = () => {
   return (
     <div className="bg-brand-black/50 sticky top-0 mx-auto flex w-full max-w-7xl items-center justify-between p-8 text-white shadow-md backdrop-blur-lg">
       <div>logo</div>
-      <ul>
-        {/* <NextLink>
-			  
-		  </NextLink> */}
+      <ul className="flex items-center space-x-6">
+        <NextLink href="/">Home</NextLink>
+        <NextLink href="/">Blog</NextLink>
       </ul>
     </div>
   );
