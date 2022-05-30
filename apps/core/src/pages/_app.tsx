74151dd2a4188fa2142872fd138f87b02dce34de
Darryl Brooks <darrylbrooks13@gmail.com>
2022-05-30 10:23:13 -0500

    refactor: husky linting issues again

--- apps/core/src/pages/_app.tsx
+++ apps/core/src/pages/_app.tsx
@@ -1,8 +1,6 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
-)
+const MyApp = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
 
-export default MyApp
+export default MyApp;
