e57e7f4c053b65010fe70ca9b70ab17b0afd44ed
Garrett Tolbert <garrett1.tolbert@gmail.com>
2022-05-30 17:54:43 -0500

    feat: added avatar to header

--- apps/blog/next.config.js
+++ apps/blog/next.config.js
@@ -4,4 +4,7 @@ const withTM = require('next-transpile-modules')(['ui', 'utils', 'types']);
 
 module.exports = withTM({
   reactStrictMode: true,
+  images: {
+    domains: ['avatars.githubusercontent.com'],
+  },
 });
--- apps/blog/src/pages/index.tsx
+++ apps/blog/src/pages/index.tsx
@@ -1,40 +1,5 @@
-import type { NextPage } from 'next';
-import { useSession, signOut, signIn } from 'next-auth/react';
-import Head from 'next/head';
-import { useRouter } from 'next/router';
-import { classNames } from 'utils';
-import trpc from '@/lib/trpc';
+import { NextPage } from 'next';
 
-const Home: NextPage = () => {
-  const { query } = useRouter();
-  const { data: session, status } = useSession();
-  const { data } = trpc.useQuery(['user.getUserById', { id: (query.id as string) || '1' }]);
-  const test = false;
-  if (status === 'loading') return <div>Loading...</div>;
-
-  if (!session) {
-    return (
-      <div className={classNames(test ? 'text-red-500' : 'text-white')}>
-        Not signed in <br />
-        <button onClick={() => signIn('github')}>Sign in</button>
-      </div>
-    );
-  }
-
-  return (
-    <div>
-      <Head>
-        <title>Next.js TRPC Template</title>
-        <meta name="description" content="TRPC Template for Next.js" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className="h-[400px] bg-green-500">
-        <h1 className="text-3xl font-bold text-white">{data?.user?.name}</h1>
-        <p>{data?.user?.email}</p>
-        <button onClick={() => signOut()}>Signout</button>
-      </div>
-    </div>
-  );
-};
+const Home: NextPage = () => <div>hi</div>;
 
 export default Home;
