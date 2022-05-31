d9316d474f2c0a1c6e23492ea7053942535fceb7
Darryl Brooks <darrylbrooks13@gmail.com>
2022-05-31 09:06:59 -0500

    fix(tailwind): updating the scrollbar css for dynamic val

--- apps/core/src/components/organisms/Pillars.tsx
+++ apps/core/src/components/organisms/Pillars.tsx
@@ -4,6 +4,17 @@ import { useEffect, useRef, useState } from 'react';
 const Pillars = () => {
   const scrollingContainer = useRef<HTMLDivElement>(null);
   const [pos, setPos] = useState(0);
+  const scrollBarConfig = [
+    {
+      top: 'top-0',
+    },
+    {
+      top: 'top-[39%]',
+    },
+    {
+      top: 'top-[75%]',
+    },
+  ];
 
   useEffect(() => {
     scrollingContainer.current?.scrollTo({
@@ -20,7 +31,7 @@ const Pillars = () => {
           {/* HStack */}
           <div className="flex h-full w-full">
             {/* Stack */}
-            <div className="relative mr-4 flex w-max flex-col items-end justify-evenly border-r border-slate-400">
+            <div className="relative mr-4 flex w-max flex-col items-end justify-between ">
               {pillars.map((pillar, idx) => (
                 <div
                   key={pillar.label}
@@ -29,22 +40,20 @@ const Pillars = () => {
                     setPos(idx);
                   }}
                 >
-                  <p className="mt-2 pr-2 text-4xl text-slate-300 hover:text-purple-400">
+                  <p className="mt-2 pr-2 align-bottom text-4xl text-slate-300 hover:text-purple-400">
                     {pillar.label}
                   </p>
                   <div className="absolute top-0 right-0 h-full w-0.5 bg-[#909090]" />
                 </div>
               ))}
               <div
-                className={`absolute top-${
-                  pos === 0 ? '0' : `[${pos * 33}%]`
-                } h-2/6 w-0.5 bg-purple-400 transition-all duration-500`}
+                className={`absolute ${scrollBarConfig[pos].top} h-1/4 w-0.5 bg-purple-400 transition-all duration-500`}
               />
             </div>
             {/* Stack */}
             <div className="h-[207px] overflow-y-hidden" ref={scrollingContainer}>
               {pillars.map(pillar => (
-                <div key={pillar.heading} className="h-[207px] px-4">
+                <div key={pillar.description} className="h-[207px] px-4">
                   <h2 className="text-4xl text-white">{pillar.heading}</h2>
                   <p className="text-xl text-white">{pillar.description}</p>
                 </div>
