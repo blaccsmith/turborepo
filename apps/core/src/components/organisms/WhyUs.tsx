894e4d819cf362c7b10e85c1a0f505328a757126
Darryl Brooks <darrylbrooks13@gmail.com>
2022-05-30 19:14:39 -0500

    feat: adding why us component

---
+++ apps/core/src/components/organisms/WhyUs.tsx
@@ -0,0 +1,38 @@
+const whyUsContent: { heading: string; subHeading: string }[] = [
+  {
+    heading: '500+',
+    subHeading: 'Members',
+  },
+  {
+    heading: '24 / 7',
+    subHeading: 'relaxed space',
+  },
+  {
+    heading: '13+',
+    subHeading: 'Shared Projects',
+  },
+  {
+    heading: '$0',
+    subHeading: 'to join',
+  },
+];
+const WhyUs = () => (
+  <div className="min-w-screen flex min-h-screen items-center justify-center">
+    <div className="flex flex-col items-center">
+      <h1 className="mb-40 text-4xl font-medium text-white">Why us?</h1>
+      <div className="grid w-full grid-cols-2 grid-rows-2 gap-20">
+        {whyUsContent.map(({ heading, subHeading }) => (
+          <div
+            key={heading}
+            className="flex w-40 flex-col text-center transition-all duration-200 hover:translate-y-[-9px] hover:translate-x-[-9px] hover:shadow-[6px_6px_rgba(123,97,255,1)]"
+          >
+            <h2 className="text-6xl font-bold text-white">{heading}</h2>
+            <p className="text-xl text-white ">{subHeading}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  </div>
+);
+
+export default WhyUs;
--- apps/core/src/pages/index.tsx
+++ apps/core/src/pages/index.tsx
@@ -1,11 +1,13 @@
 import type { NextPage } from 'next';
 import Landing from '@components/organisms/Landing';
 import Pillars from '@components/organisms/Pillars';
+import WhyUs from '@components/organisms/WhyUs';
 
 const Home: NextPage = () => (
   <div className="h-full w-auto bg-[#212121]">
     <Landing />
     <Pillars />
+    <WhyUs />
   </div>
 );
 
