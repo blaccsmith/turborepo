23f003b8493363d8c707f89d3515ed04a531a137
Garrett Tolbert <garrett1.tolbert@gmail.com>
2022-05-31 21:25:44 -0500

    feat: added trpc mutation to new post page

--- apps/blog/src/pages/new.tsx
+++ apps/blog/src/pages/new.tsx
@@ -1,36 +1,46 @@
 import Head from 'next/head';
 import PostForm from '@/components/orgnaisms/PostForm';
+import { trpc } from '@/lib/trpc';
+import { useRouter } from 'next/router';
+import toast from 'react-hot-toast';
 
-const New = () => (
-  <>
-    <Head>
-      <title>New Post - BLACC</title>
-    </Head>
+const New = () => {
+  const router = useRouter();
 
-    <h1 className="text-2xl font-semibold tracking-tight text-white md:text-3xl">New post</h1>
+  const addPostMutation = trpc.useMutation('post.add', {
+    onError: error => {
+      toast.error(`Something went wrong: ${error.message}`);
+    },
+  });
 
-    <div className="mt-6 text-white">
-      <PostForm
-        isSubmitting={false}
-        //   isSubmitting={addPostMutation.isLoading}
-        defaultValues={{
-          title: '',
-          content: '',
-        }}
-        backTo="/"
-        onSubmit={
-          (values: { title: string; content: string }) => values
-          // console.log({ values });
-          // addPostMutation.mutate(
-          //   { title: values.title, content: values.content },
-          //   {
-          //     onSuccess: data => router.push(`/post/${data.id}`),
-          //   },
-          // );
-        }
-      />
-    </div>
-  </>
-);
+  return (
+    <>
+      <Head>
+        <title>New Post - BLACC</title>
+      </Head>
+
+      <h1 className="text-2xl font-semibold tracking-tight text-white md:text-3xl">New post</h1>
+
+      <div className="mt-6 text-white">
+        <PostForm
+          isSubmitting={addPostMutation.isLoading}
+          defaultValues={{
+            title: '',
+            content: '',
+          }}
+          backTo="/"
+          onSubmit={values => {
+            addPostMutation.mutate(
+              { title: values.title, content: values.content },
+              {
+                onSuccess: data => router.push(`/post/${data.id}`),
+              },
+            );
+          }}
+        />
+      </div>
+    </>
+  );
+};
 
 export default New;
