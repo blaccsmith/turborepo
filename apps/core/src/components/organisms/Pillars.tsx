131e4965c202558bafb0a681057be45d9327f44d
Darryl Brooks <darrylbrooks13@gmail.com>
2022-05-31 21:25:33 -0500

    feat: init responsive design

--- apps/core/src/components/organisms/Pillars.tsx
+++ apps/core/src/components/organisms/Pillars.tsx
@@ -4,15 +4,26 @@ import { useEffect, useRef, useState } from 'react';
 const Pillars = () => {
   const scrollingContainer = useRef<HTMLDivElement>(null);
   const [pos, setPos] = useState(0);
-  const scrollBarConfig = [
+  const scrollBarMobileConfig = [
     {
-      top: 'top-0',
+      left: 'left-0',
     },
     {
-      top: 'top-[39%]',
+      left: 'left-[39%]',
     },
     {
-      top: 'top-[75%]',
+      left: 'left-[75%]',
+    },
+  ];
+  const scrollBarDesktopConfig = [
+    {
+      top: 'md:top-0 md:left-auto',
+    },
+    {
+      top: 'md:top-[39%] md:left-auto',
+    },
+    {
+      top: 'md:top-[75%] md:left-auto',
     },
   ];
 
@@ -26,12 +37,12 @@ const Pillars = () => {
   return (
     <div className="min-w-screen flex min-h-screen items-center justify-center">
       <div className="flex flex-col items-center">
-        <h1 className="mb-40 text-4xl font-medium text-white">What we&apos;re about</h1>
-        <div className="h-4/5 w-4/5">
+        <h1 className="mb-10 md:mb-40 text-4xl font-medium text-white">What we&apos;re about</h1>
+        <div className="h-4/5 w-full md:w-4/5">
           {/* HStack */}
-          <div className="flex h-full w-full">
+          <div className="flex flex-col md:flex-row h-full ">
             {/* Stack */}
-            <div className="relative mr-4 flex w-max flex-col items-end justify-between ">
+            <div className="relative md:mr-4 flex w-full md:flex-col text-center items-end justify-between">
               {pillars.map((pillar, idx) => (
                 <div
                   key={pillar.label}
@@ -40,22 +51,24 @@ const Pillars = () => {
                     setPos(idx);
                   }}
                 >
-                  <p className="mt-2 pr-2 align-bottom text-4xl text-slate-300 hover:text-purple-400">
+                  <p className="mt-2 md:pr-2 align-top md:align-bottom text-lg md:text-4xl text-slate-300 hover:text-purple-400">
                     {pillar.label}
                   </p>
-                  <div className="absolute top-0 right-0 h-full w-0.5 bg-[#909090]" />
+                  <div className="absolute bottom-0 left-0 w-full md:top-0 md:right-0 md:h-full md:left-auto md:bottom-auto h-0.5 md:w-0.5 bg-[#909090]" />
                 </div>
               ))}
               <div
-                className={`absolute ${scrollBarConfig[pos].top} h-1/4 w-0.5 bg-purple-400 transition-all duration-500`}
+                className={`absolute ${scrollBarMobileConfig[pos].left}  ${scrollBarDesktopConfig[pos].top} h-0.5 md:h-1/4 md:w-0.5 w-1/4 bg-purple-400 transition-all duration-500`}
               />
             </div>
             {/* Stack */}
             <div className="h-[207px] overflow-y-hidden" ref={scrollingContainer}>
               {pillars.map(pillar => (
-                <div key={pillar.description} className="h-[207px] px-4">
-                  <h2 className="text-4xl text-white">{pillar.heading}</h2>
-                  <p className="text-xl text-white">{pillar.description}</p>
+                <div key={pillar.description} className="h-[207px] py-4 md:py-0 md:px-4">
+                  <div className="text-center mb-3 md:mb-0">
+                    <h2 className="text-3xl md:text-4xl  text-white">{pillar.heading}</h2>
+                  </div>
+                  <p className="text-lg md:text-xl text-white">{pillar.description}</p>
                 </div>
               ))}
             </div>
