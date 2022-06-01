3c1a87134d57af4ab5aa0461df7cce28386105f6
Garrett Tolbert <garrett1.tolbert@gmail.com>
2022-05-31 21:25:30 -0500

    fix: updated auth config

--- apps/blog/src/lib/auth.ts
+++ apps/blog/src/lib/auth.ts
@@ -1,23 +1,76 @@
 import { NextAuthOptions } from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Role } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 const authOptions: NextAuthOptions = {
-  session: {
-    strategy: 'jwt',
-    maxAge: 30 * 24 * 60 * 60, // 30 days
-  },
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
+      authorization: 'https://github.com/login/oauth/authorize?scope=read:user+user:email+read:org',
+      userinfo: {
+        url: 'https://api.github.com/user',
+        async request({ client, tokens }) {
+          // Get base profile
+          // @ts-ignore
+          const profile = await client.userinfo(tokens);
+
+          // If user has email hidden, get their primary email from the GitHub API
+          if (!profile.email) {
+            const emails = await (
+              await fetch('https://api.github.com/user/emails', {
+                headers: {
+                  Authorization: `token ${tokens.access_token}`,
+                },
+              })
+            ).json();
+
+            if (emails?.length > 0) {
+              // Get primary email
+              profile.email = emails.find((email: any) => email.primary)?.email;
+              // And if for some reason it doesn't exist, just use the first
+              if (!profile.email) profile.email = emails[0].email;
+            }
+          }
+
+          return profile;
+        },
+      },
     }),
   ],
+  callbacks: {
+    async session({ session, user }) {
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: user.id,
+          role: user.role,
+        },
+      };
+    },
+  },
   secret: process.env.NEXTAUTH_SECRET,
   adapter: PrismaAdapter(prisma),
 };
 
 export default authOptions;
+
+declare module 'next-auth' {
+  interface Session {
+    user: {
+      id: string;
+      name: string;
+      email: string;
+      image?: string | null;
+      role: Role;
+    };
+  }
+
+  interface User {
+    role: Role;
+  }
+}
