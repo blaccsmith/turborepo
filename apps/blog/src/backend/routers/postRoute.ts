8569c31117d90cbffc77b74f91fdb412b18e10d1
Garrett Tolbert <garrett1.tolbert@gmail.com>
2022-05-31 21:24:49 -0500

    feat: added route to add posts

---
+++ apps/blog/src/backend/routers/postRoute.ts
@@ -0,0 +1,33 @@
+import { TRPCError } from '@trpc/server';
+import { z } from 'zod';
+import createRouter from '@/backend/utils/createRouter';
+import { markdownToHtml } from '@/lib/editor';
+
+const userRoute = createRouter()
+  .middleware(async ({ ctx, next }) => {
+    if (!ctx.session) throw new TRPCError({ code: 'UNAUTHORIZED' });
+    return next();
+  })
+  .mutation('add', {
+    input: z.object({
+      title: z.string().min(1),
+      content: z.string().min(1),
+    }),
+    async resolve({ ctx, input }) {
+      const post = await ctx.prisma.post.create({
+        data: {
+          title: input.title,
+          content: input.content,
+          contentHtml: markdownToHtml(input.content),
+          author: {
+            connect: {
+              id: ctx.session!.user.id,
+            },
+          },
+        },
+      });
+      return post;
+    },
+  });
+
+export default userRoute;
