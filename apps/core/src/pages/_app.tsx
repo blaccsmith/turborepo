91cfe53092f6ab2dce0de7cdc196d949a662c171
Darryl Brooks <darrylbrooks13@gmail.com>
2022-06-03 19:02:20 -0500

    refactor: formatting and moving seo for poc

--- apps/core/src/pages/_app.tsx
+++ apps/core/src/pages/_app.tsx
@@ -5,44 +5,48 @@ import RootLayout from 'ui/components/atoms/Layouts/RootLayout';
 import { DefaultSeo } from 'next-seo';
 
 const MyApp = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => (
-  <SessionProvider session={session}>
-    <RootLayout>
-      <DefaultSeo
-        title="The Black Coder Community"
-        additionalLinkTags={[
+  <>
+    <DefaultSeo
+      title="The Black Coder Community"
+      description="A community exemplifying black excellence and pushing the needle within tech"
+      additionalLinkTags={[
+        {
+          rel: 'icon',
+          href: `https://www.blacc.xyz/favicon.ico`,
+        },
+        {
+          rel: 'apple-touch-icon',
+          href: `https://www.blacc.xyz/logo.png`,
+          sizes: '76x76',
+        },
+      ]}
+      openGraph={{
+        type: 'website',
+        url: 'https://www.blacc.xyz/',
+        title: 'BLACC',
+        site_name: 'The Black Coder Community',
+        images: [
           {
-            rel: 'icon',
-            href: `https://www.blacc.xyz/favicon.ico`,
+            url: 'https://www.blacc.xyz/banner.png',
+            width: 800,
+            height: 450,
+            alt: 'BLACC Banner',
+            type: 'image/png',
           },
-          {
-            rel: 'apple-touch-icon',
-            href: `https://www.blacc.xyz/logo.png`,
-            sizes: '76x76',
-          },
-        ]}
-        openGraph={{
-          type: 'website',
-          url: 'https://www.blacc.xyz/',
-          title: 'BLACC',
-          site_name: 'The Black Coder Community',
-          images: [
-            {
-              url: 'https://www.blacc.xyz/banner.png',
-              width: 800,
-              height: 450,
-              alt: 'BLACC Banner',
-              type: 'image/png',
-            },
-          ],
-        }}
-        twitter={{
-          handle: '@blaccxyz_',
-          cardType: 'summary_large_image',
-        }}
-      />
-      <Component {...pageProps} />
-    </RootLayout>
-  </SessionProvider>
+        ],
+      }}
+      twitter={{
+        handle: '@blaccxyz_',
+        cardType: 'summary_large_image',
+        site: '@blaccxyz_',
+      }}
+    />
+    <SessionProvider session={session}>
+      <RootLayout>
+        <Component {...pageProps} />
+      </RootLayout>
+    </SessionProvider>
+  </>
 );
 
 export default MyApp;
