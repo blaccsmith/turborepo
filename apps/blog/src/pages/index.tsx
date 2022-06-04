7fa1426a44cfbc79ff7fc41fbb9330b0043dfe49
Garrett Tolbert <garrett1.tolbert@gmail.com>
2022-06-04 17:20:27 -0500

    fix: fixing eslint issues

--- apps/blog/src/pages/index.tsx
+++ apps/blog/src/pages/index.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/no-array-index-key */
+/* eslint-disable no-nested-ternary */
 import { NextPage } from 'next';
 import { useSession } from 'next-auth/react';
 import dynamic from 'next/dynamic';
