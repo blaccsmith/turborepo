8983840bdc1c1af5344d9b5e8c4fc0fe566611b8
Garrett Tolbert <garrett1.tolbert@gmail.com>
2022-06-05 17:00:13 -0500

    refactor: added tag to loading state

--- apps/blog/src/components/atoms/Skeletons/PostSummarySkeleton.tsx
+++ apps/blog/src/components/atoms/Skeletons/PostSummarySkeleton.tsx
@@ -3,6 +3,7 @@ const PostSummarySkeleton = () => (
     <div className="w-full">
       <div className="mb-2 h-5 w-5/6 rounded-md  bg-gray-500 md:w-2/5" />
       <div className="h-4 w-2/3  rounded-md bg-gray-500 md:w-1/3" />
+      <div className="mt-4 h-[26px] w-1/3 rounded-md bg-gray-600" />
     </div>
     <div className="flex w-full items-center space-x-6 text-white md:w-min">
       <div className="mb-1 h-4 w-9 rounded-md bg-gray-500" />
