1711e4bd40013bd8676c9efcecbaa463c2b9f11e
Garrett Tolbert <garrett1.tolbert@gmail.com>
2022-06-09 19:16:35 -0500

    SSR edit page

--- apps/blog/src/pages/p/[slug]/edit.tsx
+++ apps/blog/src/pages/p/[slug]/edit.tsx
@@ -2,88 +2,80 @@ import { useSession } from 'next-auth/react';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import toast from 'react-hot-toast';
-import { trpc } from '@/lib/trpc';
+import { InferQueryOutput, transformer, trpc } from '@/lib/trpc';
 import PostForm from '@/components/orgnaisms/PostForm';
 import Layout from '@/components/molecules/SearchLayout';
+import { appRouter } from '@/backend/routers';
+import { createSSGHelpers } from '@trpc/react/ssg';
+import { GetServerSideProps } from 'next';
+import safeJsonStringify from 'safe-json-stringify';
+import { createContext } from '@/backend/utils/context';
 
-const EditPostPage = () => {
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const ssg = createSSGHelpers({ router: appRouter, ctx: await createContext(), transformer });
+
+  const queryRes = await ssg.fetchQuery('post.detail', {
+    slug: params?.slug as string,
+  });
+
+  const post = JSON.parse(safeJsonStringify(queryRes));
+  return { props: { post } };
+};
+
+type PostDetail = {
+  post: InferQueryOutput<'post.detail'>;
+};
+
+const EditPostPage = ({ post }: PostDetail) => {
   const { data: session } = useSession();
   const router = useRouter();
-  const postQuery = trpc.useQuery(['post.detail', { slug: router.query.slug as string }]);
   const editPostMutation = trpc.useMutation('post.edit', {
     onError: error => {
       toast.error(`Something went wrong: ${error.message}`);
     },
   });
 
-  if (postQuery.data) {
-    const postBelongsToUser = postQuery.data.author.id === session!.user.id;
-
-    return (
-      <>
-        <Head>
-          <title>Edit {postQuery.data.title} - Beam</title>
-        </Head>
-
-        {postBelongsToUser ? (
-          <>
-            <h1 className="text-2xl font-semibold tracking-tight text-white md:text-3xl">
-              Edit &quot;{postQuery.data.title}&quot;
-            </h1>
-
-            <div className="my-6 text-white">
-              <PostForm
-                isSubmitting={editPostMutation.isLoading}
-                defaultValues={{
-                  title: postQuery.data.title,
-                  tags: postQuery.data.tags.map(el => el.tag.id),
-                  content: postQuery.data.content,
-                }}
-                backTo={`/p/${postQuery.data.slug}`}
-                onSubmit={values => {
-                  editPostMutation.mutate(
-                    {
-                      id: postQuery.data.id,
-                      data: { ...values },
-                    },
-                    {
-                      onSuccess: ({ slug }) => router.push(`/p/${slug}`),
-                    },
-                  );
-                }}
-              />
-            </div>
-          </>
-        ) : (
-          <div>You don&apos;t have permissions to edit this post.</div>
-        )}
-      </>
-    );
-  }
-
-  if (postQuery.isError) {
-    return <div>Error: {postQuery.error.message}</div>;
-  }
+  const postBelongsToUser = post.author.id === session?.user.id;
 
   return (
-    <div className="animate-pulse">
-      <div className="h-9 w-3/4 rounded bg-gray-500" />
-      <div className="mt-7">
-        <div>
-          <div className="h-5 w-10 rounded bg-gray-500" />
-          <div className="border-secondary mt-2 h-[42px] rounded border" />
-        </div>
-        <div className="mt-6">
-          <div className="h-5 w-10 rounded bg-gray-500" />
-          <div className="border-secondary mt-2 h-9 rounded border" />
-          <div className="border-secondary mt-2 h-[378px] rounded border" />
-        </div>
-      </div>
-      <div className="mt-9 flex gap-4">
-        <div className="h-button w-[92px] rounded-full bg-gray-500" />
-        <div className="h-button border-secondary w-20 rounded-full border" />
-      </div>
-    </div>
+    <>
+      <Head>
+        <title>Edit {post.title} - Beam</title>
+      </Head>
+
+      {postBelongsToUser ? (
+        <>
+          <h1 className="text-2xl font-semibold tracking-tight text-white md:text-3xl">
+            Edit &quot;{post.title}&quot;
+          </h1>
+
+          <div className="my-6 text-white">
+            <PostForm
+              isSubmitting={editPostMutation.isLoading}
+              defaultValues={{
+                title: post.title,
+                tags: post.tags.map(el => el.tag.id),
+                content: post.content,
+              }}
+              backTo={`/p/${post.slug}`}
+              onSubmit={values => {
+                editPostMutation.mutate(
+                  {
+                    id: post.id,
+                    data: { ...values },
+                  },
+                  {
+                    onSuccess: ({ slug }) => router.push(`/p/${slug}`),
+                  },
+                );
+              }}
+            />
+          </div>
+        </>
+      ) : (
+        <div>You don&apos;t have permissions to edit this post.</div>
+      )}
+    </>
   );
 };
 
