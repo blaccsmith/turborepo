5ba37a1f62e1be7f11f7d446dea3162b7c38c9a3
Darryl Brooks <darrylbrooks13@gmail.com>
2022-06-12 23:37:11 -0500

    feat: removing main and updating core

--- apps/core/next.config.js
+++ apps/core/next.config.js
@@ -3,6 +3,14 @@
 const withTM = require('next-transpile-modules')(['ui', 'utils', 'types']);
 
 module.exports = withTM({
+  async rewrites() {
+    return [
+      {
+        source: '/blog/:match*',
+        destination: 'https://blog.blacc.xyz/:match*',
+      },
+    ];
+  },
   reactStrictMode: true,
   images: {
     domains: ['images.unsplash.com', 'pbs.twimg.com'],
