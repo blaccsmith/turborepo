dc90f9dde8b353127bf7285180d43f7a507b6125
Darryl Brooks <darrylbrooks13@gmail.com>
2022-06-12 23:15:36 -0500

    refactor: updating redirects

--- apps/main/next.config.js
+++ apps/main/next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:match*',
+        destination: 'https://core.blacc.xyz/:match*',
+        permanent: true,
+      },
+      {
+        source: '/blog/:match*',
+        destination: 'https://blog.blacc.xyz/:match*',
+        permanent: true,
+      },
+    ];
+  },
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
