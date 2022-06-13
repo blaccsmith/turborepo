8c6ae091ab41740bd8aca59469821c6677788afb
Garrett Tolbert <garrett1.tolbert@gmail.com>
2022-06-13 01:58:01 -0500

    updated testimonials

--- apps/core/src/components/organisms/Testimonials.tsx
+++ apps/core/src/components/organisms/Testimonials.tsx
@@ -1,35 +1,16 @@
-import Image from 'next/image';
+import NextImage from 'ui/components/atoms/NextImage';
+import { classNames } from 'utils';
 
 const testimonals: { text: string; avatar: string; handle: string }[] = [
   {
-    text: 'This is such a dope space for me to learn new things',
-    avatar: 'https://pbs.twimg.com/profile_images/1462081803745775623/T539s4P6_400x400.jpg',
-    handle: '@DBredvick',
+    text: "It's a unique community and I like some of the answers that I can get",
+    avatar: 'https://avatars.githubusercontent.com/u/87236699?v=4',
+    handle: 'Diogenes of Toronto',
   },
   {
-    text: 'This is such a dope space for me to learn new things',
-    avatar: 'https://pbs.twimg.com/profile_images/1462081803745775623/T539s4P6_400x400.jpg',
-    handle: '@DBredvick',
-  },
-  {
-    text: 'This is such a dope space for me to learn new things',
-    avatar: 'https://pbs.twimg.com/profile_images/1462081803745775623/T539s4P6_400x400.jpg',
-    handle: '@DBredvick',
-  },
-  {
-    text: 'This is such a dope space for me to learn new things',
-    avatar: 'https://pbs.twimg.com/profile_images/1462081803745775623/T539s4P6_400x400.jpg',
-    handle: '@DBredvick',
-  },
-  {
-    text: 'This is such a dope space for me to learn new things',
-    avatar: 'https://pbs.twimg.com/profile_images/1462081803745775623/T539s4P6_400x400.jpg',
-    handle: '@DBredvick',
-  },
-  {
-    text: 'This is such a dope space for me to learn new things',
-    avatar: 'https://pbs.twimg.com/profile_images/1462081803745775623/T539s4P6_400x400.jpg',
-    handle: '@DBredvick',
+    text: 'The community is very helpful and no one is judged based on their skilled level.',
+    avatar: '',
+    handle: 'adamsantana',
   },
 ];
 
@@ -40,31 +21,34 @@ const Testimonials = () => (
         What people are saying about us
       </h1>
       <div className="flex h-4/5 justify-center md:block md:w-11/12">
-        <div className="grid grid-rows-3 gap-5 md:grid-cols-3 md:grid-rows-2 md:gap-10">
+        <div className="grid grid-cols-1 gap-5 md:grid-cols-3 md:grid-rows-2 md:gap-10">
           {testimonals.map(({ text, avatar, handle }, idx) => (
             <div
-              className={`relative h-[154px] rounded-lg md:ml-0 md:w-11/12 ${
+              className={`relative rounded-lg ${
                 idx % 2 === 0 ? 'bg-[rgba(123,97,255,0.2)]' : ''
-              } border-2 border-purple-400 p-9 md:shadow-[-9px_0px_#7B61FF]`}
+              } border-brand-purple-400 flex h-max flex-col space-y-3 border-2 p-6 md:shadow-[-9px_0px_#7B61FF]`}
               key={handle}
             >
-              <div className="absolute top-5 left-2">
+              <div className="absolute top-2 left-2">
                 <h2 className="text-5xl text-white">“</h2>
               </div>
-              <div className="absolute bottom-5 left-5 flex">
-                <Image
-                  src={avatar}
-                  layout="fixed"
-                  width={24}
-                  height={24}
-                  style={{
-                    borderRadius: '50%',
-                  }}
-                />
-                <p className="ml-2 text-white">{handle}</p>
-              </div>
-              <div className="w-full">
-                <p className="text-white">{text}</p>
+              <p className="text-white">{text}</p>
+              <div className="flex space-x-2">
+                <div
+                  className={classNames(
+                    !avatar
+                      ? 'border-brand-purple-500 flex items-center justify-center border text-xs text-white'
+                      : '',
+                    'relative h-6 w-6 overflow-hidden rounded-full',
+                  )}
+                >
+                  {!avatar ? (
+                    <p>{handle[0].toUpperCase()}</p>
+                  ) : (
+                    <NextImage src={avatar} layout="fill" />
+                  )}
+                </div>
+                <p className="text-white">{handle}</p>
               </div>
             </div>
           ))}
