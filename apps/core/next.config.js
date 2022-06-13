e8b33340b29364970c1d9c1a6d3eb26490ad2704
Darryl Brooks <darrylbrooks13@gmail.com>
2022-06-13 17:33:38 -0500

    Revert "feat: setting up vercel config"
    This reverts commit 3a10c4769da545dcf9ba59322907d2ac9ae08556.

--- apps/core/next.config.js
+++ apps/core/next.config.js
@@ -3,15 +3,6 @@
 const withTM = require('next-transpile-modules')(['ui', 'utils', 'types']);
 
 module.exports = withTM({
-  async redirects() {
-    return [
-      {
-        source: '/blog/:match*',
-        destination: 'https://blog.blacc.xyz/:match*',
-        permanent: true,
-      },
-    ];
-  },
   reactStrictMode: true,
   images: {
     domains: ['images.unsplash.com', 'pbs.twimg.com'],
