51b3fe4361efafb3d1734d4d484e726865e4dd9e
Garrett Tolbert <garrett1.tolbert@gmail.com>
2022-06-13 00:24:32 -0500

    feat: added umami script

--- apps/core/src/pages/index.tsx
+++ apps/core/src/pages/index.tsx
@@ -3,6 +3,7 @@ import Landing from '@components/organisms/Landing';
 import Pillars from '@components/organisms/Pillars';
 import WhyUs from '@components/organisms/WhyUs';
 import Testimonials from '@components/organisms/Testimonials';
+import Script from 'next/script';
 
 const Home: NextPage = () => (
   <div className="relative h-auto w-auto bg-[#212121]">
@@ -10,6 +11,12 @@ const Home: NextPage = () => (
     <Pillars />
     <WhyUs />
     <Testimonials />
+    <Script
+      async
+      defer
+      data-website-id="e375e4f2-5380-4f39-8ee1-c64ba5b374cc"
+      src="https://core.up.railway.app/umami.js"
+    />
   </div>
 );
 
