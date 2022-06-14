ef699036044dfb2cb904742db01c182fe3a0a795
Darryl Brooks <darrylbrooks13@gmail.com>
2022-06-13 21:35:09 -0500

    chore: removing match

--- apps/main/next.config.js
+++ apps/main/next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: '/',
-        destination: 'https://www.core.blacc.xyz/:match*',
+        destination: 'https://www.core.blacc.xyz',
         permanent: true,
         basePath: false,
       },
