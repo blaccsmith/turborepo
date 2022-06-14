9409205e55236c8f31896a298f739a02b5087796
Darryl Brooks <darrylbrooks13@gmail.com>
2022-06-13 23:33:33 -0500

    chore: updating daily pic

--- apps/core/src/components/organisms/ProjectSpark.tsx
+++ apps/core/src/components/organisms/ProjectSpark.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import NextImage from 'ui/components/atoms/NextImage';
-import questions from 'ui/assets/qa.png';
+import tribe from 'ui/assets/tribe.png';
 
 const ProjectSpark = () => (
   <div className="flex h-screen flex-col items-center justify-center text-white">
-    <NextImage
-      objectFit="cover"
-      className="z-10 rounded-xl"
-      alt="jumbotron image"
-      src={questions}
-    />
+    <NextImage objectFit="cover" className="z-10 rounded-xl" alt="jumbotron image" src={tribe} />
     <div className="flex w-full scale-105 p-4" />
   </div>
 );
