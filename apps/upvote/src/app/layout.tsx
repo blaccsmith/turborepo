b92c8aa7a11418bb6619cbe663ef26127ed6b96d
Charles Eugene Loubao <charlesloubao95@gmail.com>
2023-01-22 14:42:04 -0500

    Pulling events

--- apps/upvote/src/app/layout.tsx
+++ apps/upvote/src/app/layout.tsx
@@ -1,24 +1,31 @@
 import './globals.css';
 import Navigation from './ui/navigation';
+import {SupabaseClient} from "@supabase/supabase-js";
 
 const getEvents = async () => {
-  return ['event1', 'event2'];
+    const supabaseClient = new SupabaseClient(process.env.SUPABASE_URL!, process.env.SUPABASE_ANON_KEY!)
+    return supabaseClient.from("events")
+        .select(`id, name`)
+        .then(({data, error}) => {
+            if (error) throw error
+            return data!.map<string>(event => event.name)
+        })
 };
 
-export default async function RootLayout({ children }: { children: React.ReactNode }) {
-  const events = await getEvents();
+export default async function RootLayout({children}: { children: React.ReactNode }) {
+    const events = await getEvents();
 
-  return (
-    <html lang="en">
-      {/*
+    return (
+        <html lang="en">
+        {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
-      <body>
-        <Navigation events={events} />
+        <head/>
+        <body>
+        <Navigation events={events}/>
         {children}
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
